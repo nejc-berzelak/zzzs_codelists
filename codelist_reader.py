17793dce39816e08b7960f2dcb44c33c5acdfd11
Nejc Berzelak <id1@nejc-b.net>
2025-03-30 22:25:57 +0200

    Dopolnitev branja metapodatkov iz imena datoteke

--- codelist_reader.py
+++ codelist_reader.py
@@ -6,6 +6,17 @@ def read_xls_codelist(file, filename_metadata=True, sheets=None):
     filename_metadata: if True, the function attempts to read metadata from filename.
     """
     
+    # Try to get metadata from filename.
+    if filename_metadata:
+        pattern = r'_([0-9]{2})_([0-9]{4})'
+        match = re.search(pattern, file)
+        if match:
+            month, year = match.groups()
+            month = int(month)
+            year = int(year)
+        else:
+            raise ValueError("Filename does not contain valid year and month metadata.")
+
     for sheet in sheets:
         # Read the sheet into a DataFrame
         df = pd.read_excel(file, sheet_name=sheet, header=None)
@@ -16,7 +27,6 @@ def read_xls_codelist(file, filename_metadata=True, sheets=None):
                 header_row = i
                 break
             
-            
         if header_row is not None:
             # Set the header row as the column names
             df.columns = df.iloc[header_row]
@@ -26,36 +36,4 @@ def read_xls_codelist(file, filename_metadata=True, sheets=None):
             raise ValueError(f"Naslovne vrstice ni bilo mogoče najti na listu {sheet}.")
 
 
-read_xls_codelist("/Users/nejcb/WD/Sifranti_Cistopis_objava_11_2025 (1).xlsx", sheets=["Š 6"])
 
-
-
-
-def extract_metadata_from_filename(filename):
-    """
-    Extract year and month metadata from the filename.
-    Assumes the filename contains the pattern '_MM_YYYY'.
-    """
-    pattern = r'_([0-9]{2})_([0-9]{4})'
-    match = re.search(pattern, filename)
-    if match:
-        month, year = match.groups()
-        return int(year), int(month)
-    else:
-        raise ValueError("Filename does not contain valid year and month metadata.")
-
-# Example usage
-filename = "Sifranti_Cistopis_objava_11_2025 (1).xlsx"
-year, month = extract_metadata_from_filename(filename)
-print(f"Year: {year}, Month: {month}")
-
-
-
-
-# df = pd.read_excel("/Users/nejcb/WD/Sifranti_Cistopis_objava_11_2025 (1).xlsx", 
-#                    sheet_name="Š 4", header=None)
-
-# for i, row in  enumerate(df.values):
-#     if row[0] == "Šifra":
-#         print(i)
-    
