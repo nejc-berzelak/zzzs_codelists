82b4506035263fdc39d46f8de69398e82f692085
Nejc Berzelak <id1@nejc-b.net>
2025-03-30 22:08:26 +0200

    Osnutek funkcije za uvoz iz Excela

---
+++ codelist_reader.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def read_xls_codelist(file, filename_metadata=True, sheets=None):
+    """
+    Read a codelist from Excel file and return a dataframe.
+    filename_metadata: if True, the function attempts to read metadata from filename.
+    """
+    
+    for sheet in sheets:
+        # Read the sheet into a DataFrame
+        df = pd.read_excel(file, sheet_name=sheet, header=None)
+        
+        # Find the header in a codelist Excel sheet. Code lists start on different rows in different sheets. 
+        for i, row in enumerate(df.values):
+            if row[0] == "Šifra":
+                header_row = i
+                break
+            
+            
+        if header_row is not None:
+            # Set the header row as the column names
+            df.columns = df.iloc[header_row]
+            df = df[header_row + 1:]
+            return df
+        else:
+            raise ValueError(f"Naslovne vrstice ni bilo mogoče najti na listu {sheet}.")
+
+
+
+
+
+
+
+
+# df = pd.read_excel("/Users/nejcb/WD/Sifranti_Cistopis_objava_11_2025 (1).xlsx", 
+#                    sheet_name="Š 4", header=None)
+
+# for i, row in  enumerate(df.values):
+#     if row[0] == "Šifra":
+#         print(i)
+    
