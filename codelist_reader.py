d68b005ea5f1051b4f3463b4fee6724e4eca45e7
Nejc Berzelak <id1@nejc-b.net>
2025-03-31 16:04:21 +0200

    Odstranjeno branje metapodatkov iz imena datoteke.

--- codelist_reader.py
+++ codelist_reader.py
@@ -1,22 +1,12 @@
 import pandas as pd
 
-def read_xls_codelist(file, filename_metadata=True, sheets=None):
+def read_xls_codelist(file, sheets=None):
     """
     Read a codelist from Excel file and return a dataframe.
     filename_metadata: if True, the function attempts to read metadata from filename.
     """
     
     # Try to get metadata from filename.
-    if filename_metadata:
-        pattern = r'_([0-9]{2})_([0-9]{4})'
-        match = re.search(pattern, file)
-        if match:
-            month, year = match.groups()
-            month = int(month)
-            year = int(year)
-        else:
-            raise ValueError("Filename does not contain valid year and month metadata.")
-
     for sheet in sheets:
         # Read the sheet into a DataFrame
         df = pd.read_excel(file, sheet_name=sheet, header=None)
@@ -36,4 +26,5 @@ def read_xls_codelist(file, filename_metadata=True, sheets=None):
             raise ValueError(f"Naslovne vrstice ni bilo mogoče najti na listu {sheet}.")
 
 
+read_xls_codelist()
 
